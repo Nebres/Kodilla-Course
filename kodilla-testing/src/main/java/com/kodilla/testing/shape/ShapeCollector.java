02d3f2e0e988762ff3e905f38e11240aef4d1824
Nebres <serbenski@o2.pl>
2018-06-17 23:17:45 +0200

    zadanie 6_4

--- kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -1,4 +1,27 @@
 package com.kodilla.testing.shape;
 
+import java.util.ArrayList;
+
 public class ShapeCollector {
+
+    private final static ArrayList<Shape> shapeList = new ArrayList<Shape>();
+
+    public void addFigure(Shape shape) {
+        shapeList.add(shape);
+    }
+
+    public void removeFigure(Shape shape) {
+        shapeList.remove(shape);
+    }
+
+    public Shape getFigure(int i) {
+        return shapeList.get(i);
+    }
+
+    public void showFigures() {
+        for (Shape shape : shapeList) {
+            System.out.println(shape.getShapeName());
+        }
+    }
+
 }
