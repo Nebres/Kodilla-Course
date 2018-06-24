8eb1888b9b854da1068a9500b981f487c2c4bcc4
Nebres <serbenski@o2.pl>
2018-06-24 19:48:26 +0200

    zadanie6_6 | same testy

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,19 +1,7 @@
 package com.kodilla.testing;
 
-
-import com.kodilla.testing.calculator.Calculator;
-
 public class TestingMain {
     public static void main(String[] args){
-
-        int add = new Calculator().runAdd(3,4);
-        int substraction = new Calculator().runSubstraction(3,4);
-
-        if (add == 7 && substraction == -1) {
-            System.out.println("All ok");
-        } else {
-            System.out.println("error");
-        }
-
     }
+
 }
