ecfb3424729b10502664c6ef8ef7a9cdcd40174d
Nebres <serbenski@o2.pl>
2018-06-26 22:16:31 +0200

    zadanie7_1 | porawiłem metody 'upiekszajace'; przepraszam za smieci w commicie ale namieszalem w tym branchu bawiac sie gitgui :|

--- kodilla-stream/src/main/java/StreamMain.java
+++ kodilla-stream/src/main/java/StreamMain.java
@@ -6,10 +6,9 @@ public class StreamMain {
 
         PoemBeautifier beautifier = new PoemBeautifier();
 
-        beautifier.beautify("example text", (text) -> text.toUpperCase());
-        beautifier.beautify("another text", (text) -> text.substring(8).replace('t', 'T'));
-        beautifier.beautify("one more time", (text) -> "".concat("..::||" + text + "||::.."));
-
+        beautifier.beautify("example text", text -> text.toUpperCase());
+        beautifier.beautify("another text", text -> text.substring(text.length() < 3 ? 0 : text.length()/ 2 + 2).replace('t', 'T'));
+        beautifier.beautify("one more time", text -> "".concat("..::||" ).concat(text).concat("||::.."));
     }
 
 }
