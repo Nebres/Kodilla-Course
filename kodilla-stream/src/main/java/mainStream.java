249344981aa3ffb2069d727b0444602837cfea82
Nebres <serbenski@o2.pl>
2018-06-28 14:39:32 +0200

    master | fix

---
+++ kodilla-stream/src/main/java/mainStream.java
@@ -0,0 +1,14 @@
+import com.kodilla.stream.array.ArrayOperations;
+
+import java.util.stream.IntStream;
+
+public class mainStream implements ArrayOperations {
+
+    private final static int[] ARRAY = new int[] { 2, 55, 31, 22, 16, 14, 88, 23, 56, 72, 62,
+            33, 84, 90, 12, 45, 56, 31, 74, 19 };
+
+    public static void main(String[] args) {
+        System.out.println(ArrayOperations.getAverage(ARRAY));
+    }
+
+}
