a333844486df2fbae5c2cbd2869d429ff1744604
Nebres <serbenski@o2.pl>
2018-07-02 19:58:15 +0200

    zadanie 8_2 | first commit

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -4,7 +4,7 @@ public class FirstChallenge {
 
     public double divide(double a, double b) throws ArithmeticException {
         if (b == 0) {
-            throw new ArithmeticException("Davide by 0");
+            throw new ArithmeticException("Divide by 0");
         }
         return a / b;
     }
@@ -16,17 +16,14 @@ public class FirstChallenge {
     public static void main(String[] args) {
 
         FirstChallenge firstChallenge = new FirstChallenge();
-        double result = firstChallenge.divide(3.0, 1.0);
 
         try {
-            double testedResult = firstChallenge.divide(3.0, 0.0);
+            double result = firstChallenge.divide(3.0, 0.0);
         } catch (ArithmeticException e) {
             System.out.println("Error " + e.getMessage());
         } finally {
-            System.out.println("Never Divine by 0");
+            System.out.println("Never Divide by 0");
         }
-
-        System.out.println(result);
     }
 
 }
