58b39e8c42c3d0b0aec93e6f26bfebd40b211b51
Nebres <serbenski@o2.pl>
2018-07-04 20:53:04 +0200

    zadanie9_1 | wprowadziłem statyczna zmiena, zlikwidowalem sufixy i prefixy, kosmetyczne poprawki

--- kodilla-good-patterns/src/main/java/MainRunner.java
+++ kodilla-good-patterns/src/main/java/MainRunner.java
@@ -7,13 +7,16 @@ import java.util.stream.Collectors;
 
 public class MainRunner {
 
+    private static final String SEPARATOR = " ! ";
+
     public static void main(String[] args){
 
        Map<String, List<String>> listMap = new MovieStore().getMovies();
 
-       String result = listMap.values().stream()
+       String result = listMap.values()
+               .stream()
                .flatMap(s -> s.stream())
-               .collect(Collectors.joining(" ! "," "," "));
+               .collect(Collectors.joining(SEPARATOR));
 
        System.out.println(result);
     }
