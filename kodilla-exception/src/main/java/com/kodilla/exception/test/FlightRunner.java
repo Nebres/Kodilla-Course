bb04bfc03426b46b2b1b9cee6d9aad368da823c7
Nebres <serbenski@o2.pl>
2018-07-06 18:18:20 +0200

    zadanie8_4 | zlikwidowałem zbędne zagnieżdżenie w try... catch... ; poprawki kosmetyczne

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightRunner.java
@@ -5,26 +5,19 @@ import java.util.Map;
 
 public class FlightRunner {
 
-
     public static void main(String[] args) {
 
-        Flight flight = new Flight("Chopin", "Heathrow" );
+        Flight flight = new Flight("Chopin", "Heathrow");
         Flight flight2 = new Flight("Charles de Gaulle", "Chopin");
 
         try {
             Map<String, Boolean> flightMap = new MapCreator().initializeMapOfFlights();
-            try {
-                new FlightFinder(flightMap).findFlight(flight);
-            } catch (RouteNotFoundException r) {
-                r.printStackTrace();
-            }
-            try {
-                new FlightFinder(flightMap).findFlight(flight2);
-            } catch (RouteNotFoundException r) {
-                r.printStackTrace();
-            }
+            new FlightFinder(flightMap).findFlight(flight);
+            new FlightFinder(flightMap).findFlight(flight2);
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (RouteNotFoundException r) {
+            r.printStackTrace();
         }
     }
 
