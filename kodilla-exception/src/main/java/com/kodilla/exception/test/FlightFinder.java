52129f0a23b3a184e32dc3c5d73e830c99b2e9ef
Nebres <serbenski@o2.pl>
2018-07-08 14:40:52 +0200

    zadanie8_4 | Zamieniłem concat() na +. Kosmetyczne zmiany

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -6,7 +6,7 @@ public class FlightFinder {
 
     private final Map<String, Boolean> mapOfFlights;
     private static final String ERROR_COMMUNICATE = "There is no flight from %s Airport to %s Airport check if you get "
-            .concat("correct departure point or contact with our call center (+48 121 12 12)");
+            + "correct departure point or contact with our call center (+48 121 12 12)";
 
     public FlightFinder(Map<String, Boolean> mapOfFlights) {
         this.mapOfFlights = mapOfFlights;
@@ -14,9 +14,10 @@ public class FlightFinder {
 
     public void findFlight(Flight flight) throws RouteNotFoundException {
         if (!mapOfFlights.containsKey(flight.getDepartureAirport())) {
-            throw new RouteNotFoundException(String.format(ERROR_COMMUNICATE, flight.getDepartureAirport(),flight.getArrivalAirport()));
+            throw new RouteNotFoundException(String.format(ERROR_COMMUNICATE, flight.getDepartureAirport(), flight.getArrivalAirport()));
         }
-        System.out.println(flight.getDepartureAirport().concat(" | ").concat(mapOfFlights.get(flight.getDepartureAirport()).toString()));
+        System.out.println(flight.getDepartureAirport().concat(" | ")
+                .concat(mapOfFlights.get(flight.getDepartureAirport()).toString()));
    }
 
 }
