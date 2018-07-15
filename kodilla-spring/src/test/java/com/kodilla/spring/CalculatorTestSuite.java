1942324414b00e563d3861780cfd1d40cb8bf11a
Nebres <serbenski@o2.pl>
2018-07-15 21:32:52 +0200

    zadanie10_2 | poprawiłem metody tak by zwracały wyniki; zrobiłem testy które sparwdzają prawidłowosc wyników

--- kodilla-spring/src/test/java/com/kodilla/spring/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/CalculatorTestSuite.java
@@ -1,6 +1,7 @@
 package com.kodilla.spring;
 
 import com.kodilla.spring.calculator.Calculator;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,17 +14,52 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class CalculatorTestSuite {
 
     @Test
-    public void TestDisplay() {
-
+    public void TestDisplayAdd() {
         //Given
         ApplicationContext context =
                 new AnnotationConfigApplicationContext("com.kodilla.spring");
         Calculator calculator = context.getBean(Calculator.class);
+        double expected = 1.00;
         //When
-        calculator.add(0.50,0.50);
-        calculator.div(0.50,0.50);
-        calculator.sub(0.50,0.50);
-        calculator.mul(0.50,0.50);
+        double actual = calculator.add(0.50,0.50);
+        //Than
+        Assert.assertEquals(expected, actual, 1e-2);
+    }
+
+    @Test
+    public void TestDisplaySub() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        double expected = 0.00;
+        //When
+        double actual = calculator.sub(0.50,0.50);
+        Assert.assertEquals(expected, actual, 1e-2);
+    }
+
+    @Test
+    public void TestDisplayMul() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        double expected = 0.25;
+        //When
+        double actual = calculator.mul(0.50,0.50);
+        Assert.assertEquals(expected, actual, 1e-2);
+    }
+
+    @Test
+    public void TestDisplayDiv() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        double expected = 1.00;
+        //When
+        double actual = calculator.div(0.50,0.50);
+        Assert.assertEquals(expected, actual, 1e-2);
     }
 
 }
