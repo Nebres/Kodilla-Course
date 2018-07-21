f3ae9a5791afcaa0b359c08423012a5dea7d379a
Nebres <serbenski@o2.pl>
2018-07-21 22:18:34 +0200

    zadanie9_4 | final

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/thirdChallenge/Shop.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/thirdChallenge/Shop.java
@@ -3,5 +3,5 @@ package com.kodilla.good.patterns.challenges.thirdChallenge;
 public interface Shop {
 
     String getInformationAboutCompany();
-    void process(String product, double quality);
+    void process(String product, double quantity);
 }
