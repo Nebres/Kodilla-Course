6360e12ff32f78b77a2f3d88bc81cb3a785ab461
Nebres <serbenski@o2.pl>
2018-09-24 18:01:54 +0200

    task20_1 | zadanie przykładowe z kursu

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/Authenticator.java
@@ -0,0 +1,15 @@
+package com.kodilla.patterns2.facade;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+@Component
+public class Authenticator {
+
+    public boolean isAuthenticated(Long userId) {
+        Random generator = new Random ();
+        return generator.nextBoolean();
+    }
+
+}
